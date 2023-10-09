ed66748ae2020287ab0dc0565e8d018fa0178fdc
Christian <frezzebrain42@gmail.com>
2023-10-08 21:04:08 -0600

    Audio Player Design & JSON structure completed.

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+data = [
+    {
+        1: {
+            "title": "Lost in the City Lights",
+            "artist": "Cosmo Sheldrake",
+            "audio_url": "../audio/lost-in-city-lights-145038.mp3",
+            "image_url": "../img/cover-1.png"
+        },
+        2: {
+            "title": "Forest Lullaby",
+            "artist": "Lesfm",
+            "audio_url": "../audio/forest-lullaby-110624.mp3",
+            "image_url": "../img/cover-2.png"
+        }
+    }
+]
